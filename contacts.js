e068ddd3416bfaacc6b60dfb45dfbd9d465aeea5
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-02-11 03:31:30 +0200

    adds step 2 and 3

---
+++ contacts.js
@@ -0,0 +1,50 @@
+const fs = require('fs/promises')
+const path = require('path')
+const { randomUUID } = require("crypto");
+
+const contactsPath = path.resolve('./db/contacts.json')
+
+async function listContacts() {
+    const list = await fs.readFile(contactsPath, "utf-8");
+    console.log(JSON.parse(list))
+}
+
+async function getContactById(contactId) {
+    const list = await fs.readFile(contactsPath, "utf-8");
+    const filter = JSON.parse(list).filter(item => item.id === contactId)
+    console.log(filter)
+}
+
+async function removeContact(contactId) {
+    const list = await fs.readFile(contactsPath, "utf-8");
+    const filter = JSON.parse(list).filter(item => item.id !== contactId)
+    fs.writeFile(contactsPath, JSON.stringify(filter), "utf8")
+    console.log(filter)
+    
+}
+
+async function addContact(name, email, phone) {
+    
+    const newContact = {
+     id: randomUUID(),
+        name,
+        email,
+        phone,
+    }
+    const list = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+    list.push( newContact)
+
+  
+fs.writeFile(contactsPath, JSON.stringify(list), "utf8")
+     console.log(list)
+}
+
+
+
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+}
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("process.argv")
+const contacts = require('./contacts');
+
+//  contacts.listContacts()
+//  contacts.getContactById('2')
+contacts.removeContact('4')
+ contacts.addContact("serg","q@sdfg", "555555555")
+
